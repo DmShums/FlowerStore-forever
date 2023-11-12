d9601d9c1d648aa0f06c3b296fc725a65bddc4e1
Dmytro Shumsky <schooldima1@gmail.com>
2023-11-12 13:48:01 +0200

    fix tests

--- src/main/java/ua/edu/ucu/apps/flowerstore/decorator/BasketDecorator.java
+++ src/main/java/ua/edu/ucu/apps/flowerstore/decorator/BasketDecorator.java
@@ -1,39 +1,16 @@
-//package ua.edu.ucu.apps.flowerstore.decorator;
-//import ua.edu.ucu.apps.flowerstore.flowers.Item;
-//
-//public class BasketDecorator extends ItemDecorator{
-//    private final int decorPrice = 4;
-//    private Item item;
-//    @Override
-//    public double price() {
-//        return item.price() + decorPrice;
-//    }
-//
-//    @Override
-//    public double getDescription() {
-//        return 0;
-//    }
-//}
 package ua.edu.ucu.apps.flowerstore.decorator;
-import ua.edu.ucu.apps.flowerstore.flowers.Item;
 
 public class BasketDecorator extends ItemDecorator {
     private final int decorPrice = 4;
-    private Object item;  // Change the type to Object
+    private PricedItem item;
 
-    // Updated constructor to accept any object with a 'price()' method
-    public BasketDecorator(Object item) {
+    public BasketDecorator(PricedItem item) {
         this.item = item;
     }
 
     @Override
     public double price() {
-        // Check if the item has a 'price()' method
-        if (item instanceof Item) {
-            return ((Item) item).price() + decorPrice;
-        } else {
-            throw new UnsupportedOperationException("Item must have a 'price()' method");
-        }
+        return item.price() + decorPrice;
     }
 
     @Override
--- src/test/java/ua/edu/ucu/apps/flowerstore2/BasketDecoratorTest.java
+++ src/test/java/ua/edu/ucu/apps/flowerstore2/BasketDecoratorTest.java
@@ -1,8 +1,8 @@
 package ua.edu.ucu.apps.flowerstore2;
 
 import org.junit.jupiter.api.Test;
-import ua.edu.ucu.apps.flowerstore.flowers.Item;
 import ua.edu.ucu.apps.flowerstore.decorator.BasketDecorator;
+import ua.edu.ucu.apps.flowerstore.decorator.PricedItem;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,7 +11,7 @@ public class BasketDecoratorTest {
     @Test
     public void testBasketDecoratorPrice() {
         // Create a mock item with a known price
-        MockItem mockItem = new MockItem(10.0);
+        PricedItem mockItem = () -> 10.0;
 
         // Create a BasketDecorator for the mock item
         BasketDecorator basketDecorator = new BasketDecorator(mockItem);
@@ -19,23 +19,5 @@ public class BasketDecoratorTest {
         // Check if the total price is the sum of the item's price and the decorPrice
         double expectedPrice = 10.0 + basketDecorator.price();
         assertEquals(expectedPrice, basketDecorator.price(), 0.001);
-    }
-
-    // MockItem class for testing purposes
-    private static class MockItem {
-
-        private double price;
-
-        public MockItem(double price) {
-            this.price = price;
-        }
-
-        public double price() {
-            return price;
-        }
-
-        public double getDescription() {
-            return 0;
-        }
     }
 }
